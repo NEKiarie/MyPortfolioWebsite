06a15f51d03929575a713bb50af94f14ae25ad00
+ <+>
2022-12-24 12:28:34 +0300

    1st Commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./about.css";
+import AboutImg from "../../assets/images/preview.jpg";
+import Info from "./Info";
+import MyResume from "../../assets/Myresume.pdf";
+
+const About = () => {
+  return (
+    <section className="about section" id="#about">
+      <h2 className="section__title">About Me</h2>
+      <span className="section__subtitle">My Introduction</span>
+
+      <div className="about__container container grid">
+        <img src={AboutImg} alt="AboutImg" className="about__img" />
+        <div className="about__data">
+        <Info />
+
+        <p className="about__description">
+          A full-stack Developer. I create web pages with UI/UX user interface.
+          I have years of experience and many clients are happy with the
+          projects carried out.
+        </p>
+        <a download="" href={MyResume} className="button button--flex">
+          Download Resume 📄
+        </a>
+
+      </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/home/Data.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h3 className="home__subtitle">Hello👋, I am</h3>
+      <h1 className="home__title"> Elvis Njiiri.</h1>
+      <h3 className="home__subtitle"> A Full-Stack Web/Software Developer.</h3>
+      <p className="home__description">
+      I'm a creative developer based in Kenya, and I'm passionate and dedicated to my work.
+      </p>
+
+      <a href="#contact" className="button button--flex">
+        Say Hello 📥 
+      </a>
+    </div>
+  );
+};
+
+export default Data;
